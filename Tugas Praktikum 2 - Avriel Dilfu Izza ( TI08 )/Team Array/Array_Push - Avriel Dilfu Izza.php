82bcd047a67479f4da1db57c81ea43ea97a7ee75
ELSKUII <92253022+ELSKUII@users.noreply.github.com>
2022-02-28 21:09:23 +0700

    Add files via upload

---
+++ Tugas Praktikum 2 - Avriel Dilfu Izza ( TI08 )/Team Array/Array_Push - Avriel Dilfu Izza.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+	$name = ['ELSKUI', 'Avriel', 'Dilfu', 'Izza'];
+	array_push($name, 'EL');
+	foreach($name as $person) {
+		echo $person . '<br/>';
+	}
+?>
+</body>
+</html>
---
+++ Tugas Praktikum 2 - Avriel Dilfu Izza ( TI08 )/Team Variabel/Variabel_User - Avriel Dilfu Izza.php
@@ -0,0 +1,11 @@
+<?php
+	// definisikan variables
+	$nama = 'Avriel Dilfu Izza';
+	$umur = 19;
+	$berat = 62;
+	
+	echo 'Nama : ' . $nama;
+	echo '<br/>Umur : ' . $umur . ' Tahun';
+	echo '<br/>Berat : ' . $berat . ' Kg';
+	echo "<br/>Hai Haii $nama Apa kabar kamu?!";
+?>
